125f99210963f7862ec243dff145b936d78a299c
Spryti <67284103+Spryti@users.noreply.github.com>
2021-07-02 21:00:11 +0000

    Setup

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-// Author:
+// Author:Jordan Muturi
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let textP2;
+let buttonDiv;
+let upButton;
+let downButton;
+let leftButton;
+let rightButton;
+
 
 // Global ML variables
-//let featureExtractor;
+let featureExtractor;
+let imgFeatures;
+let knnClassifier;
+let video;
+let isModelReady;
+let ups;
+let downs;
+let lefts;
+let rights;
+
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model is loading, please wait...");
+  textP.parent(textDiv);
+  textP2 = createP("[Training data here.]");
+  textP2.parent(textDiv);
+  buildButtons();
+  ups = 0;
+  downs = 0;
+  lefts = 0;
+  rights = 0;
+  video = createCapture(VIDEO, videoReady);
 }
 
 function draw() {
