2307aa92e542270b1a6a762e4353aa17906704d9
Spryti <67284103+Spryti@users.noreply.github.com>
2021-07-06 18:22:45 +0000

    Finished Project

--- script.js
+++ script.js
@@ -43,21 +43,70 @@ function setup() {
 }
 
 function draw() {
-
+  if(isModelReady) {
+    translate(width, 0);
+    scale(-1, 1);
+    image(video, 0, 0);
+    imgFeatures = featureExtractor.infer(canvas);
+    if(knnClassifier.getNumLabels() > 0) {
+      knnClassifier.classify(imgFeatures, gotResults);
+    }
+  }
 }
 
 function buildButtons() {
-
+  buttonDiv = createDiv();
+  //up button
+  upButton = createButton("Up");
+  upButton.parent(buttonDiv);
+  upButton.mousePressed(function() {
+    ups++;
+    textP2.html("Ups: " + ups + " - Downs " + downs + " - Lefts: " + lefts +  " - Rights: " + rights);
+    knnClassifier.addExample(imgFeatures, "Up");
+  });
+  //down button
+  downButton = createButton("Down");
+  downButton.parent(buttonDiv);
+  downButton.mousePressed(function() {
+    downs++;
+    textP2.html("Ups: " + ups + " - Downs " + downs + " - Lefts: " + lefts +  " - Rights: " + rights);
+    knnClassifier.addExample(imgFeatures, "Down");
+  });
+  //left button
+  leftButton = createButton("Left");
+  leftButton.parent(buttonDiv);
+  leftButton.mousePressed(function() {
+    lefts++;
+    textP2.html("Ups: " + ups + " - Downs " + downs + " - Lefts: " + lefts +  " - Rights: " + rights);
+    knnClassifier.addExample(imgFeatures, "Left");
+  });
+  //right button
+  rightButton = createButton("Right");
+  rightButton.parent(buttonDiv);
+  rightButton.mousePressed(function() {
+    rights++;
+    textP2.html("Ups: " + ups + " - Downs " + downs + " - Lefts: " + lefts +  " - Rights: " + rights);
+    knnClassifier.addExample(imgFeatures, "Right");
+  });
+  buttonDiv.style("display", "none");
 }
 
 function videoReady() {
-
+  video.style("display", "none");
+  featureExtractor = ml5.featureExtractor("MobileNet", featureExtractorLoaded);
 }
 
 function featureExtractorLoaded() {
-
+  isModelReady = true;
+  knnClassifier = ml5.KNNClassifier();
+  textP.html("Begin adding examples!");
+  buttonDiv.style("display", "block");
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  } else {
+    textP.html("Label: " + results.label);
+  }
 }
